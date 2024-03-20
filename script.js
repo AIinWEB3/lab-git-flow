96d6d05d3cf6bcc83b108241240448937a562198
zoeyj <zoeyj8170@gmail.com>
2024-03-20 13:39:45 -0700

    add poem

--- script.js
+++ script.js
@@ -19,7 +19,14 @@ const poems = [
     author: "Genesis",
     poem: "Roses are red \n Violets are blue \n Duck is using phone \n That looks like you",
     image: "duck.jpeg",
+  },
+
+  {
+    author:" Rita Dove",
+    poem:"Billie Holidays burned voice \n had as many shadows as lights, \n a mournful candelabra against a sleek piano, \n the gardenia her signature under that ruined face. \n (Now youre cooking, drummer to bass \n magic spoon, magic needle. \n Take all day if you have to \n with your mirror and your bracelet of song. \n Fact is, the invention of women under siege \n has been to sharpen love in the service of myth. \n If you cant be free, be a mystery.",
+    image: "bee.jpeg"
   }
+
 ];
 
 let displayPoems = (array) => {
